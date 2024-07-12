a4bb302107d4d0034ff80de176efec386a97d1f0
MaskedGod <codegobbledygook@gmail.com>
2024-07-12 18:19:46 +0400

    -l line counter command added

--- ccwc.py
+++ ccwc.py
@@ -6,6 +6,10 @@ def main():
     args = parser.parse_args()
     if args.b:
         print(bytes_counter(args))
+    elif args.l:
+        print(lines_counter(args))
+    else:
+        parser.print_help()
 
 
 parser = ArgumentParser(
@@ -15,10 +19,18 @@ parser = ArgumentParser(
     )
 parser.add_argument('filename')
 parser.add_argument("-b", action="store_true", help="Counts bytes in a file")
+parser.add_argument("-l", action="store_true", help="Counts lines in a file")
 
 
 def bytes_counter(args):
     result = os.path.getsize(args.filename)
+    return result
+
+def lines_counter(args):
+    result = 0
+    with open(args.filename, 'r', encoding='UTF-8') as file:
+        for _ in file.readlines():
+            result += 1
     return result
 
 
