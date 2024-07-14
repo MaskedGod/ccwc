32d610749dba1e3453a7a00381ae80534f87e6ef
MaskedGod <codegobbledygook@gmail.com>
2024-07-15 01:36:46 +0400

    added stndin input if no file name provided, simple unittest for every function

--- ccwc.py
+++ ccwc.py
@@ -5,17 +5,20 @@ from rich import print
 
 def main():
     args = parser.parse_args()
-
-    if args.b:
-        print(bytes_counter(args))
-    elif args.l:
-        print(lines_counter(args))
-    elif args.w:
-        print(words_counter(args))
-    elif args.c:
-        print(characters_counter(args)) 
+    if args.filename:
+        fname = args.filename
     else:
-        print(lines_counter(args), words_counter(args), bytes_counter(args), sep=" ",)
+        fname = sys.stdin.readline().split('\n')[0]
+    if args.b:
+        print(bytes_counter(fname))
+    elif args.l:
+        print(lines_counter(fname))
+    elif args.w:
+        print(words_counter(fname))
+    elif args.c:
+        print(characters_counter(fname)) 
+    else:
+        print(lines_counter(fname), words_counter(fname), bytes_counter(fname), sep=" ",)
 
 
 parser = ArgumentParser(
@@ -23,38 +26,38 @@ parser = ArgumentParser(
     description="Counts bytes, lines, words, characters in a file",
     epilog="help placeholder"
     )
-filename_stdin = sys.stdin.readline().split('\n')[0]
-parser.add_argument('filename', nargs='?', default=filename_stdin, help="The name of the file to process. If not provided, input is read from stdin.")
+
+parser.add_argument('filename', nargs='?', default=None, help="The name of the file to process. If not provided, input is read from stdin.")
 parser.add_argument("-b", action="store_true", help="Counts bytes in a file")
 parser.add_argument("-l", action="store_true", help="Counts lines in a file")
 parser.add_argument("-w", action="store_true", help="Counts words in a file")
 parser.add_argument("-c", action="store_true", help="Counts characters in a file")
 
 
-def bytes_counter(args):
+def bytes_counter(fname):
     result = 0
-    with open(args.filename, 'rb') as file:
+    with open(fname, 'rb') as file:
         result = len(file.read())
     return result
 
-def lines_counter(args):
+def lines_counter(fname):
     result = 0
-    with open(args.filename, 'r', encoding='utf-8') as file:
+    with open(fname, 'r', encoding='utf-8') as file:
         for _ in file.readlines():
             result += 1
     return result
 
-def words_counter(args):
+def words_counter(fname):
     result = 0
-    with open(args.filename, 'r', encoding='utf-8') as file:
+    with open(fname, 'r', encoding='utf-8') as file:
         for row in file.readlines():
             words_amount = len(row.split())
             result += words_amount
     return result
 
-def characters_counter(args):
+def characters_counter(fname):
     result = 0
-    with open(args.filename, 'r', encoding='utf-8') as file:
+    with open(fname, 'r', encoding='utf-8') as file:
         for row in file.read():
             characters_amount = len(row)
             result += characters_amount
--- test_ccws.py
+++ test_ccws.py
@@ -1,2 +1,33 @@
 import subprocess
 import unittest
+
+
+class TestWordCounter(unittest.TestCase):
+
+    def test_byte_count(self):
+        process = subprocess.run('py ccwc.py -b test.txt', shell=True, capture_output=True, text=True)
+        self.assertEqual(process.stdout.strip("\n"), "342190")
+
+    def test_line_count(self):
+        process = subprocess.run('py ccwc.py -l test.txt', shell=True, capture_output=True, text=True)
+        self.assertEqual(process.stdout.strip("\n"), "7145")
+
+    def test_word_count(self):
+        process = subprocess.run('py ccwc.py -w test.txt', shell=True, capture_output=True, text=True)
+        self.assertEqual(process.stdout.strip("\n"), "58164")
+
+    def test_characters_count(self):
+        process = subprocess.run('py ccwc.py -c test.txt', shell=True, capture_output=True, text=True)
+        self.assertEqual(process.stdout.strip("\n"), "332147")
+
+    def test_default_option(self):
+        process = subprocess.run('py ccwc.py test.txt', shell=True, capture_output=True, text=True)
+        self.assertEqual(process.stdout.strip("\n"), "7145 58164 342190")
+
+    def test_standard_input(self):
+        process = subprocess.run('py ccwc.py -l', input='test.txt', shell=True, capture_output=True, text=True)
+        self.assertEqual(process.stdout.strip("\n"), "7145")
+
+
+if __name__ == '__main__':
+    unittest.main()
